a4c29b57a9df8e72f82c324b0d8d3e4e44c85d96
triggering-event <gabriel.j.shapiro@gmail.com>
2020-04-14 23:48:12 -0400

    completes MVP (github usercard component)

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,55 +1,61 @@
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
+const followersArray = ['lex-node', 'tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
 
-   Skip to Step 3.
-*/
+const userGetter = username => {
+    axios.get(`https://api.github.com/users/${username}`)
+        .then(userData => {
+            console.log(userData);
+            const userCard = cardComponent(userData.data);
+            document.querySelector('.cards').appendChild(userCard);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
 
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
+followersArray.forEach(follower => {
+    userGetter(follower);
+})
 
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
+const cardComponent = (userData) => {
+    //create & classify elements
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('card');
+    const cardImg = document.createElement('img');
+    const cardInfoDiv = document.createElement('div');
+    cardInfoDiv.classList.add('card-info');
+    const cardHeader = document.createElement('h3');
+    cardHeader.classList.add('name');
+    const cardPara1 = document.createElement('p');
+    cardPara1.classList.add('username');
+    const cardPara2 = document.createElement('p');
+    const cardPara3 = document.createElement('p');
+    const userPageLink = document.createElement('a');
+    const cardPara4 = document.createElement('p');
+    const cardPara5 = document.createElement('p');
 
-const followersArray = [];
+    //hydrate elements form data object
+    cardImg.src = userData.avatar_url;
+    cardHeader.textContent = userData[name];
+    cardPara1.textContent = userData.login;
+    cardPara2.textContent = `Location: ${userData[location]}`;
+    cardPara3.textContent = `Profile: `
+    userPageLink.href = userData.url;
+    cardPara4.textContent = `Followers: ${userData.followers}`;
+    cardPara5.textContent = `Bio: ${userData.bio}`;
 
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+    //stitch elements together
+    cardDiv.appendChild(cardImg);
+    cardDiv.appendChild(cardInfoDiv);
+    cardInfoDiv.appendChild(cardHeader);
+    cardInfoDiv.appendChild(cardPara1);
+    cardInfoDiv.appendChild(cardPara2);
+    cardInfoDiv.appendChild(cardPara3);
+    cardInfoDiv.appendChild(cardPara4);
+    cardInfoDiv.appendChild(cardPara5);
+    cardPara3.append(userPageLink);
 
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
+    //return assembled element
+    return cardDiv;
+}
 
-*/
-
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
